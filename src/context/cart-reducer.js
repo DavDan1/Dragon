a958e50dc653a9d051ad397372b6c9894c3ee006
davitdanielyan <davit.danielyan@weknowit.nu>
2022-11-25 13:40:38 +0100

    Add reducer to save cart on local storage

--- src/context/cart-reducer.js
+++ src/context/cart-reducer.js
@@ -1,4 +1,10 @@
+const storeCartItems = (cartItems) => {
+  const cart = cartItems.length > 0 ? cartItems : [];
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 export const sumItems = cartItems => {
+  storeCartItems(cartItems);
   return {
     itemCount: cartItems.reduce((total, product) => total + product.quantity, 0),
     total: cartItems.reduce((total, prod) => total + (prod.price * prod.quantity
@@ -52,6 +58,7 @@ const cartReducer = (state, action) => {
       }
 
     case 'CLEAR':
+      localStorage.removeItem('cart');
       return {
         cartItems: [],
         itemCount: 0,
