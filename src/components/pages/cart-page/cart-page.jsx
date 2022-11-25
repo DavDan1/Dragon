7bbd164dd0ca6844a6e4f83f48a5215dcea2843c
davitdanielyan <davit.danielyan@weknowit.nu>
2022-11-25 12:33:22 +0100

    Add total amount of prods to the cart

--- src/components/pages/cart-page/cart-page.jsx
+++ src/components/pages/cart-page/cart-page.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 import { CartContext } from "../../../context/cart-context";
 import Layout from "../../shared/layout";
 import CartItem from "./cart-item";
+import Total from "./total";
 import "./cart-page.styles.scss";
 
 const CartPage = () => {
   const { cartItems, itemCount, total } = useContext(CartContext);
+
   return (
     <Layout>
       <>
@@ -14,12 +16,13 @@ const CartPage = () => {
           cartItems.length === 0 ? <div className="empty-cart">Your Cart is empty</div>
             :
             <>
-              <div className="caty-page">
+              <div className="cart-page">
                 <div className="cart-item-container">
                   {
                     cartItems.map(item => <CartItem{...item} key={item.id} />)
                   }
                 </div>
+                <Total itemCount={itemCount} total={total} />
               </div>
             </>
         }
