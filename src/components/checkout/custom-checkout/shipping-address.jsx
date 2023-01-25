92dcd575d4f2bba07d41f57628f02559d84402d7
davitdanielyan <davit-danielyan@hotmail.com>
2023-01-25 15:43:46 +0100

    Create shipping address component, add formik package

---
+++ src/components/checkout/custom-checkout/shipping-address.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Formik } from "formik";
+
+const validate = values => {
+  const { name, email, address } = values;
+  const errors = {};
+  if (!name) { errors.name = 'Required' };
+  if (!email) { errors.email = 'Required' };
+  if (!address) { errors.address = 'Required' };
+
+  return errors;
+}
+
+const ShippingAddress = () => {
+  const initialValues = {
+    email: '',
+    name: '',
+    address: '',
+  }
+  return (
+    <div>
+      <h4>
+        <Formik
+          initialValues={initialValues}
+          validate={validate}
+          onSubmit={(values) => {
+            console.log('values', values)
+          }}
+        >
+          {
+            ({ values, errors, handleChange, handleSubmit }) => {
+              const { name, email, address } = errors
+              return (
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={handleChange}
+                      value={values.name}
+                      className={'nomad-input ' + (name ? 'error' : '')}
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      name="email"
+                      onChange={handleChange}
+                      value={values.name}
+                      className={'nomad-input ' + (email ? 'error' : '')}
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      name="address"
+                      onChange={handleChange}
+                      value={values.name}
+                      className={'nomad-input ' + (address ? 'error' : '')}
+                    />
+                  </div>
+                  <div className="submit-btn">
+                    <button
+                      type="submit"
+                      className="button is-black nomad-btn submit">
+                      Continue
+                    </button>
+                  </div>
+                </form>
+              )
+            }
+          }
+        </Formik>
+      </h4>
+    </div>
+  )
+}
+
+export default ShippingAddress;
